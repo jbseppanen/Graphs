2956cb78c6a63d2f1e140e2d232a61947de41052
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-13 15:53:48 -0700

    Attempts to make bfs and dfs methods work.  So far neither one is functioning correctly.

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,59 +3,137 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
+
     def add_vertex(self, vertex):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex] = set()
+
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("Does not exist!")
+
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create an empty Queue
+        q = Queue()
+        # Create an empty Visited set
+        visited = set()
+        # Add the starting vertex to the queue
+        q.enqueue(starting_vertex)
+        # While the queue is not empty...
+        while q.size() > 0:
+            # Dequeue the first vertex
+            v = q.dequeue()
+            # If it has not been visited...
+            if v not in visited:
+                # Mark it as visited (print it and add it to the visited set)
+                print(v)
+                visited.add(v)
+                # Then enqueue each of its neighbors in the queue
+                for neighbor in self.vertices[v]:
+                    q.enqueue(neighbor)
+
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
-    def dft_recursive(self, starting_vertex):
+        s = Stack()
+        visited = set()
+        s.push(starting_vertex)
+        while s.size() > 0:
+            v = s.pop()
+            if v not in visited:
+                print(v)
+                visited.add(v)
+                for neighbor in self.vertices[v]:
+                    s.push(neighbor)
+
+    def dft_recursive(self, starting_vertex, visited=None):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         This should be done using recursion.
         """
-        pass  # TODO
+        if visited is None:
+            visited = set()
+        print(starting_vertex)
+        visited.add(starting_vertex)
+        for neighbor in self.vertices[starting_vertex]:
+            if neighbor not in visited:
+                self.dft_recursive(neighbor, visited)
+
     def bfs(self, starting_vertex, destination_vertex):
         """
         Return a list containing the shortest path from
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
-    def dfs(self, starting_vertex, destination_vertex):
+        q = Queue()
+        visited = set()
+        paths = [[starting_vertex]]
+        q.enqueue(starting_vertex)
+        while q.size() > 0:
+            valid_paths = []
+            for path in paths:
+                valid_paths = []
+                last_item = path[len(path) - 1]
+                if self.vertices[last_item]:
+                    for n in self.vertices[last_item]:
+                        path.append(n)
+                        if n == destination_vertex:
+                            return path
+                        else:
+                            valid_paths.append(path)
+            paths = valid_paths
+
+
+            # Dequeue the first vertex
+            v = q.dequeue()
+            # If it has not been visited...
+            if v not in visited:
+                # Mark it as visited (print it and add it to the visited set
+                visited.add(v)
+        return None
+
+    def dfs(self, starting_vertex, destination_vertex, visited=None, path=None):
         """
         Return a list containing a path from
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        if visited is None:
+            visited = set()
+        if path is None:
+            path = []
+        visited.add(starting_vertex)
+        path.append(starting_vertex)
+        if starting_vertex == destination_vertex:
+            return path
+        if self.vertices[starting_vertex]:
+            for neighbor in self.vertices[starting_vertex]:
+                if neighbor not in visited:
+                    self.dfs(neighbor, destination_vertex, visited, path)
 
 
-
-
-
-if __name__ == '__main__':
+# if __name__ == '__main__':
+if True:
     graph = Graph()  # Instantiate your graph
     # https://github.com/LambdaSchool/Graphs/blob/master/objectives/breadth-first-search/img/bfs-visit-order.png
     graph.add_vertex(1)
@@ -80,6 +158,7 @@ if __name__ == '__main__':
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
+    print("-----vertices------")
     print(graph.vertices)
 
     '''
@@ -89,6 +168,7 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
+    print("-----DFT------")
     graph.dft(1)
 
     '''
@@ -106,6 +186,7 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
+    print("-----BFT------")
     graph.bft(1)
 
     '''
@@ -115,12 +196,14 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5
         1, 2, 4, 6, 3, 5, 7
     '''
+    print("-----DFT Recursive------")
     graph.dft_recursive(1)
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
+    print("-----BFS------")
     print(graph.bfs(1, 6))
 
     '''
@@ -128,4 +211,5 @@ if __name__ == '__main__':
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
+    print("-----DFS------")
     print(graph.dfs(1, 6))
